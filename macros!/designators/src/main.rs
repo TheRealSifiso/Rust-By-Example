9c59afdd3e7fb8a0b2bafd77b1aedafe45046486
Sifiso <sifiso1101@gmail.com>
2022-11-27 17:08:07 +0200

    Add print_result! macro

--- macros!/designators/src/main.rs
+++ macros!/designators/src/main.rs
@@ -12,11 +12,21 @@ The 'stringify!' macro converts an 'ident' into a String.
 macro_rules! create_function {
     ($func_name: ident) => {
         fn $func_name() {
-            
-            println!("You called {:?}()",
-                stringify!($func_name));
-
+            println!("You called {:?}()", stringify!($func_name));
         }
+    };
+}
+
+/*
+The following macro takes an expression of type 'expr' as an argument and
+prints it as a String along with its result.
+
+The 'expr' designator is used for expressions
+*/
+
+macro_rules! print_result {
+    ($expression: expr) => {
+        println!("{:?} = {:?}", stringify!($expression), $expression);
     };
 }
 fn main() {
